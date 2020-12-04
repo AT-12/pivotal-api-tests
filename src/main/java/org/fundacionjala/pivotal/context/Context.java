14f029a7eee8bca83802e722a563aa6ffd7858ec
Laura4lilavati <hlaura.toro@gmail.com>
2020-12-04 12:09:25 -0400

    Add Response Body Validator Class. (#13)
    Co-authored-by: Laura Montaño <Laura.Montano@fundacion-jala.org>

---
+++ src/main/java/org/fundacionjala/pivotal/context/Context.java
@@ -0,0 +1,74 @@
+package org.fundacionjala.pivotal.context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Context {
+    private Map<String, String> requestData;
+    private Map<String, String> data;
+
+    /**
+     * Constructor for the Context.
+     */
+    public Context() {
+        this.requestData = new HashMap<>();
+        this.data = new HashMap<>();
+    }
+
+    /**
+     * Saves the requestData of form data in requestData.
+     *
+     * @param requestDataToStore
+     */
+    public void saveRequestData(final Map<String, String> requestDataToStore) {
+        this.requestData = requestDataToStore;
+    }
+
+    /**
+     * Gets the value of key given.
+     *
+     * @param key
+     * @return a String data
+     */
+    public String getRequestValue(final String key) {
+        return this.requestData.getOrDefault(key, "");
+    }
+
+    /**
+     * Saves the data of form data in data.
+     *
+     * @param key
+     * @param value
+     */
+    public void saveData(final String key, final String value) {
+        this.data.put(key, value);
+    }
+
+    /**
+     * Gets the value of key given.
+     *
+     * @param key
+     * @return a String data
+     */
+    public String getValueData(final String key) {
+        return this.data.getOrDefault(key, "");
+    }
+
+    /**
+     * Gets data map.
+     *
+     * @return data map
+     */
+    public Map<String, String> getData() {
+        return data;
+    }
+
+    /**
+     * Sets data map.
+     *
+     * @param dataToSet
+     */
+    public void setData(final Map<String, String> dataToSet) {
+        this.data = dataToSet;
+    }
+}
--- src/main/java/org/fundacionjala/pivotal/utils/AuthenticationUtils.java
+++ src/main/java/org/fundacionjala/pivotal/utils/AuthenticationUtils.java
@@ -30,7 +30,24 @@ public final class AuthenticationUtils {
         requestSpecification = new RequestSpecBuilder()
             .setRelaxedHTTPSValidation()
             .addHeader(TOKEN_HEADER, PivotalEnvironment.getInstance().getToken())
+            .addHeader("Content-Type", "application/json")
             .build();
+        return requestSpecification;
+    }
+
+    /**
+     * Getter to request specifications with
+     * Token of user and the BaseURI of API which is loaded
+     * from properties file.
+     *
+     * @return requestSpecification
+     */
+    public static RequestSpecification getLoggedReqSpecWithoutContentType() {
+        RestAssured.baseURI = BASE_URL_API;
+        requestSpecification = new RequestSpecBuilder()
+                .setRelaxedHTTPSValidation()
+                .addHeader(TOKEN_HEADER, PivotalEnvironment.getInstance().getToken())
+                .build();
         return requestSpecification;
     }
 
