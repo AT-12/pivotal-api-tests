0cc99342d3dd52f75d04fe1a5ac889e33c8a27b1
roht0515 <rodrigo051599@gmail.com>
2020-12-07 17:17:36 -0400

    step definitions added

--- src/test/java/org/fundacionjala/pivotal/stepdefs/RequestStepDefs.java
+++ src/test/java/org/fundacionjala/pivotal/stepdefs/RequestStepDefs.java
@@ -8,18 +8,20 @@ import io.restassured.response.Response;
 import org.fundacionjala.core.client.RequestManager;
 import org.fundacionjala.core.utils.Mapper;
 import org.fundacionjala.core.utils.JsonSchemaValidator;
+import org.fundacionjala.pivotal.config.PivotalEnvironment;
 import org.fundacionjala.pivotal.context.Context;
 import org.fundacionjala.pivotal.utils.AuthenticationUtils;
 import org.fundacionjala.pivotal.utils.ResponseBodyValidator;
 import org.testng.Assert;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class RequestStepDefs {
 
     private Response response;
     private Context context;
-
+    private String jsonData = "";
     /**
      * Constructor of Request Step Definitions.
      *
@@ -65,7 +67,7 @@ public class RequestStepDefs {
      */
     @And("verifies response body should match with {string} JSON schema")
     public void verifiesResponseBodyMatchWithJSONSchema(final String schemaPath) {
-        JsonSchemaValidator.validate(response, schemaPath);
+        JsonSchemaValidator.validate(response, PivotalEnvironment.getInstance().getSchemasPath() + schemaPath);
     }
 
     /**
@@ -75,18 +77,91 @@ public class RequestStepDefs {
      */
     @And("verifies response should contain the following values")
     public void verifiesResponseValues(final Map<String, String> expectedValues) {
-        ResponseBodyValidator.validate(response, expectedValues);
+        ResponseBodyValidator.validateBody(response, context.getData(), expectedValues);
     }
 
+    /**
+     *
+     * @param endpoint
+     * @param body
+     */
     @When("the user sends a POST request to {string} with the following Json data")
-    public void SendsPOSTRequest(String endpoint, String body) {
+    public void sendsPOSTRequest(final String endpoint, final String body) {
         String endpointMapped = Mapper.mapValue(endpoint, context.getData());
         response = RequestManager.post(endpointMapped, body);
     }
 
+    /**
+     *
+     * @param endpoint
+     */
     @When("the user sends a GET request to {string} with the following Json data")
-    public void theUserSendsAGETRequestToWithTheFollowingJsonData(String endpoint) {
+    public void theUserSendsAGETRequestToWithTheFollowingJsonData(final String endpoint) {
         String endpointMapped = Mapper.mapValue(endpoint, context.getData());
         response = RequestManager.get(endpointMapped);
+    }
+
+    /**
+     *
+     * @param endpoint
+     */
+    @When("the user sends an invalid id to GET request to {string}")
+    public void theUserSendsAnInvalidIdToGETRequestTo(final String endpoint) {
+        String endpointMapped = Mapper.mapValue(endpoint, context.getData());
+        response = RequestManager.get(endpointMapped);
+    }
+
+    /**
+     *
+     * @param endpoint
+     */
+    @When("the user sends an invalid endpoint to GET request to {string}")
+    public void theUserSendsAnInvalidEndpointToGETRequestTo(final String endpoint) {
+        String endpointMapped = Mapper.mapValue(endpoint, context.getData());
+        response = RequestManager.get(endpointMapped);
+    }
+
+    /**
+     *
+     * @param endpoint
+     */
+    @When("the user sends a DELETE request to {string}")
+    public void theUserSendsADELETERequestTo(final String endpoint) {
+        String endpointMapped = Mapper.mapValue(endpoint, context.getData());
+        response = RequestManager.delete(endpointMapped);
+    }
+
+    /**
+     *
+     * @param jsonDataParameter
+     * @throws IOException
+     */
+    @Given("the following values in the Json data")
+    public void theFollowingValuesInTheJsonData(final String jsonDataParameter) {
+        jsonData = jsonDataParameter;
+    }
+
+    /**
+     *
+     * @param endpoint
+     * @throws IOException
+     */
+    @When("the user sends a PUT request to {string}")
+    public void theUserSendsAPUTRequestTo(final String endpoint) throws IOException {
+        String endpointMapped = Mapper.mapValue(endpoint, context.getData());
+        response = RequestManager.put(endpointMapped, jsonData);
+        context.saveData(response.asString().replace("id", "project_id"));
+    }
+
+    /**
+     *
+     * @param endpoint
+     * @throws IOException
+     */
+    @When("the user sends a POST request to {string}")
+    public void theUserSendsAPOSTRequestTo(final String endpoint) throws IOException {
+        String endpointMapped = Mapper.mapValue(endpoint, context.getData());
+        response = RequestManager.post(endpoint, jsonData);
+        context.saveData(response.asString().replace("id", "project_id"));
     }
 }
